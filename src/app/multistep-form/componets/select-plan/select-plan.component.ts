7bcdc01ec3663f40fbe15f5d8e49c9ae7ccd0cf8
Dawid Stefanko <mitow7821@o2.pl>
2023-03-22 23:00:03 +0100

    Added payment plans

--- src/app/multistep-form/componets/select-plan/select-plan.component.ts
+++ src/app/multistep-form/componets/select-plan/select-plan.component.ts
@@ -9,6 +9,30 @@ import { StepControlsService } from '../../services/step-controls.service';
 export class SelectPlanComponent {
   constructor(private stepControlService: StepControlsService) {}
 
+  paymentPlans = [
+    {
+      name: 'Arcade',
+      priceMonthly: '$9/mo',
+      priceYearly: '$90/yr',
+      yearlyDiscount: '2 months free',
+      iconName: 'icon-arcade',
+    },
+    {
+      name: 'Advanced',
+      priceMonthly: '$12/mo',
+      priceYearly: '$120/yr',
+      yearlyDiscount: '2 months free',
+      iconName: 'icon-advanced',
+    },
+    {
+      name: 'Pro',
+      priceMonthly: '$15/mo',
+      priceYearly: '$150/yr',
+      yearlyDiscount: '2 months free',
+      iconName: 'icon-pro',
+    },
+  ];
+
   get nextButtonConfig() {
     return this.stepControlService.defaultButtonConfigs.next;
   }
