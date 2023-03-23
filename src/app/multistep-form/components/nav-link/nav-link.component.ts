360cb84747475619d2a7728210d9b3aa99433587
Dawid Stefanko <dawid.stefanko@codelabs.pl>
2023-03-23 13:28:09 +0100

    Check if link could be used

--- src/app/multistep-form/components/nav-link/nav-link.component.ts
+++ src/app/multistep-form/components/nav-link/nav-link.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Step } from '../../types/steps';
 import { StepControlsService } from '../../services/step-controls.service';
+import { formGuard } from '../../utils/form-guard';
+import { SelectPlanFormService } from '../../services/select-plan-form.service';
+import { PersonalInfoFormService } from '../../services/personal-info-form.service';
 
 @Component({
   selector: 'app-nav-link',
@@ -11,9 +14,31 @@ export class NavLinkComponent {
   @Input() step!: Step;
   @Input() ordinalNumber!: number;
 
+  constructor(
+    private stepControlService: StepControlsService,
+    private personalInfoFormService: PersonalInfoFormService,
+    private selectPlanFormService: SelectPlanFormService
+  ) {}
+
   get isSelected() {
     return this.stepControlService.isCurrentStep(this.step);
   }
 
-  constructor(private stepControlService: StepControlsService) {}
+  get canActivate() {
+    const isPersonalInfoValid = this.personalInfoFormService.form.valid;
+    const isSelectPlanValid = this.selectPlanFormService.form.valid;
+
+    switch (this.step.path) {
+      case '/form/personal-info': {
+        return true;
+      }
+
+      case '/form/select-plan': {
+        return isPersonalInfoValid;
+      }
+
+      default:
+        return isPersonalInfoValid && isSelectPlanValid;
+    }
+  }
 }
